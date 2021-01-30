af57fe35c4798bbd9087344c91b89413ea5fa324
Carrie Jones <c.jessica.jones@gmail.com>
2021-01-30 18:53:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,12 +22,12 @@ class Song
   end
 
   def self.new_by_name(name)
-     self.create(name)
+     self.new(name)
  end
  
-# def self.create_by_name(name)
-#   self.new_by_name(name)
-# end
+ def self.create_by_name(name)
+   self.new_by_name(name)
+ end
 
  def self.find_by_name(name)
     @@all.find {|song| song.name == name}
