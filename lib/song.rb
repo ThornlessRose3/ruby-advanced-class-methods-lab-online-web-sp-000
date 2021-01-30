7732df9bef96c0f12415d655e395c9edffa31173
Carrie Jones <c.jessica.jones@gmail.com>
2021-01-30 18:47:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,57 @@
 class Song
+ 
   attr_accessor :name, :artist_name
-  @@all = []
+ 
+ @@all = []
 
   def self.all
     @@all
+  end
+  
+  def initialize
+    self.save
   end
 
   def save
     self.class.all << self
   end
+
+  def self.create
+    self.new
+  end
+
+  def self.new_by_name(name)
+     self.create
+     self.name = name
+ end
+ 
+# def self.create_by_name(name)
+#   self.new_by_name(name)
+# end
+
+ def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+ end
+  
+ def self.find_or_create_by_name
+    
+ end
+  
+  def self.alphabetical
+    
+  end
+  
+  def self.new_from_filename
+    
+  end
+  
+  def self.create_from_filename
+    
+  end
+  
+  def self.destroy_all
+    @@all = []
+  end
+  
 
 end
